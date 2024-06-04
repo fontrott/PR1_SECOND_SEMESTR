644f9a965972a67b42c63921fa6bababa465681d
fontrott <144059131+fontrott@users.noreply.github.com>
2024-06-04 07:40:14 +0300

    Add files via upload

--- PR4/Bus.cs
+++ PR4/Bus.cs
@@ -7,18 +7,37 @@ using System.Windows.Forms;
 
 namespace PR4
 {
-    public class Bus<T, T2, T3>
+    public class Bus<T1, T2, T3, T4>
     {
-        public T BusNumber { get; set; }
+        public T1 BusNumber { get; set; }
         public T2 DriverNameAndSurname { get; set; }
-        public T2 RouteNumber { get; set; }
-        public T3 OnTheRoute { get; set; }
-        public Bus(T busNumber, T2 driverNameAndSurname, T2 routeNumber, T3 onTheRoute)
+        public T3 RouteNumber { get; set; }
+        public T4 OnTheRoute { get; set; }
+
+        public Bus(T1 busNumber, T2 driverNameAndSurname, T3 routeNumber, T4 onTheRoute)
         {
             BusNumber = busNumber;
             DriverNameAndSurname = driverNameAndSurname;
             RouteNumber = routeNumber;
             OnTheRoute = onTheRoute;
+        }
+
+        public static void AddNewBus(List<Bus<T1, T2, T3, T4>> buses, T1 busNumber, T2 driverNameAndSurname, T3 routeNumber, T4 onTheRoute)
+        {
+            Bus<T1, T2, T3, T4> newBus = new Bus<T1, T2, T3, T4>(busNumber, driverNameAndSurname, routeNumber, onTheRoute);
+            buses.Add(newBus);
+        }
+
+        public static void UpdateBusInfoTextBox(List<Bus<T1, T2, T3, T4>> buses, TextBox busesTextBox)
+        {
+            busesTextBox.Clear();
+            foreach (Bus<T1, T2, T3, T4> b in buses)
+            {
+                bool isBusActive = (bool)(object)b.OnTheRoute;
+                string status = isBusActive ? "На маршруте" : "В парке";
+                string busInfo = $"Номер автобуса: {b.BusNumber}\r\nВодитель: {b.DriverNameAndSurname}\r\nМаршрут: {b.RouteNumber}\r\nСтатус: {status}\r\n";
+                busesTextBox.AppendText(busInfo + Environment.NewLine);
+            }
         }
     }
 }
--- PR4/Form1.cs
+++ PR4/Form1.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
@@ -14,71 +13,41 @@ namespace PR4
 {
     public partial class Buses : Form
     {
-        List<Bus<int, string, bool>> bus = new List<Bus<int, string, bool>>();
+        List<Bus<int, string, string, bool>> bus = new List<Bus<int, string, string, bool>>();
         ErrorProvider errorProvider = new ErrorProvider();
         public Buses()
         {
             InitializeComponent();
         }
-        private async void AddBus()
-        {
-            if (int.TryParse(txt_BusNumber.Text, out int busNumber) &&
-                !string.IsNullOrEmpty(txt_DriverNameAndSurname.Text) &&
-                !string.IsNullOrEmpty(txt_RouteNumber.Text))
-            {
-                bool onTheRoute = chk_OnTheRoute.Checked;
-                Bus<int, string, bool> newBus = new Bus<int, string, bool>(busNumber, txt_DriverNameAndSurname.Text, txt_RouteNumber.Text, onTheRoute);
-                bus.Add(newBus);
-                UpdateBusInformation();
-            }
-            else if (String.IsNullOrWhiteSpace(txt_BusNumber.Text))
-            {
-                errorProvider.SetError(txt_BusNumber, "Пожалуйста, введите корректные значения для номера автобуса.");
-                await Task.Delay(2000);
-                errorProvider.SetError(txt_BusNumber, "");
-            }
-            if (String.IsNullOrWhiteSpace(txt_DriverNameAndSurname.Text))
-            {
-                errorProvider.SetError(txt_DriverNameAndSurname, "Пожалуйста, введите корректные значения для имени водителя.");
-                await Task.Delay(2000);
-                errorProvider.SetError(txt_DriverNameAndSurname, "");
-            }
-            if (String.IsNullOrWhiteSpace(txt_RouteNumber.Text))
-            {
-                errorProvider.SetError(txt_RouteNumber, "Пожалуйста, введите корректные значения для номера маршрута.");
-                await Task.Delay(2000);
-                errorProvider.SetError(txt_RouteNumber, "");
-            }
-        }
-        private void UpdateBusInformation()
-        {
-            buses_TextBox.Clear();
-            foreach (Bus<int, string, bool> b in bus)
-            {
-                string status = b.OnTheRoute ? "На маршруте" : "В парке";
-                string busInfo = $"Номер автобуса: {b.BusNumber}\r\nВодитель: {b.DriverNameAndSurname}\r\nМаршрут: {b.RouteNumber}\r\nСтатус: {status}\r\n";
-                buses_TextBox.AppendText(busInfo + Environment.NewLine);
-            }
-        }
         private void btn_InThePark_Click(object sender, EventArgs e)
         {
             txt_SearchBuses.Clear();
-            List<Bus<int, string, bool>> parkedBuses = bus.Where(b => !b.OnTheRoute).ToList();
-            foreach (Bus<int, string, bool> b in parkedBuses)
+            foreach (Bus<int, string, string, bool> b in bus)
             {
                 string busInfo = $"Номер автобуса: {b.BusNumber}\r\nВодитель: {b.DriverNameAndSurname}\r\nМаршрут: {b.RouteNumber}\r\nСтатус: В парке";
                 txt_SearchBuses.AppendText(busInfo + "\r\n\r\n");
             }
         }
-        private void btn_OnTheRoute_Click(object sender, EventArgs e)
+
+        private void btn_AddBus_Click(object sender, EventArgs e)
         {
-            txt_SearchBuses.Clear();
-            List<Bus<int, string, bool>> busesOnRoute = bus.Where(b => b.OnTheRoute).ToList();
-            foreach (Bus<int, string, bool> b in busesOnRoute)
+            int busNumber;
+            if (!int.TryParse(txt_BusNumber.Text, out busNumber))
             {
-                string busInfo = $"Номер автобуса: {b.BusNumber}\r\nВодитель: {b.DriverNameAndSurname}\r\nМаршрут: {b.RouteNumber}\r\nСтатус: На маршруте";
-                txt_SearchBuses.AppendText(busInfo + "\r\n\r\n");
+                MessageBox.Show("Пожалуйста, введите корректный номер автобуса.");
+                return;
             }
+
+            string driverName = txt_DriverNameAndSurname.Text;
+
+            // Используем свойство Checked флажка для получения значения
+            bool isNewBus = chk_OnTheRoute.Checked;
+
+            string routeNumber = txt_RouteNumber.Text;
+
+            Bus<int, string, string, bool> newBus = new Bus<int, string, string, bool>(busNumber, driverName, routeNumber, isNewBus);
+
+            MessageBox.Show("Автобус успешно добавлен в список припаркованных автобусов.");
         }
         private void btn_Close1_Click(object sender, EventArgs e)
         {
@@ -96,10 +65,6 @@ namespace PR4
             txt_SearchBuses.Clear();
             buses_TextBox.Clear();
             bus.Clear();
-        }
-        private void btn_AddBus_Click(object sender, EventArgs e)
-        {
-            AddBus();
         }
         private void btn_AddBus2_Click(object sender, EventArgs e)
         {
